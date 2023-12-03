93d8ad020e21e931f4c698e485e5269f71412d4e
Toprak2 <muhammedalitoprak1@gmail.com>
2023-12-03 12:58:53 +0300

    Add a basic user interface to test bst

--- Chapter10TreeDataStructure/BinarySearchTree/src/Program.cpp
+++ Chapter10TreeDataStructure/BinarySearchTree/src/Program.cpp
@@ -1,6 +1,33 @@
+
 #include "BSTree.hpp"
 
 int main(){
+    BSTree *bstree = new BSTree();
+    int i;
+
+
+    do
+    {
+        cout<<"0)exit"<<endl;
+        cout<<"1)add element"<<endl;
+        cout<<"2)inorder list"<<endl;
+        cin>>i;
+
+        switch (i)
+        {
+        case 1:
+            cout<<"enter data to add: "<<endl;
+            int data;
+            cin>>data;
+            bstree->add(data);
+            break;
+        case 2:
+            bstree->inorderList();
+            break;
+        default:
+            break;
+        }
+    } while (i!=0);
     
     return 0;
 }
