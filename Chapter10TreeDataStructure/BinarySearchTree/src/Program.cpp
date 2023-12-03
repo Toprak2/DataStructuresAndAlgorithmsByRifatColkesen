35a28d37df71f727ecd72aa3c9e5874cbcee4ecc
Toprak2 <muhammedalitoprak1@gmail.com>
2023-12-03 13:51:45 +0300

    Add option to test remove element

--- Chapter10TreeDataStructure/BinarySearchTree/src/Program.cpp
+++ Chapter10TreeDataStructure/BinarySearchTree/src/Program.cpp
@@ -11,19 +11,31 @@ int main(){
         cout<<"0)exit"<<endl;
         cout<<"1)add element"<<endl;
         cout<<"2)inorder list"<<endl;
+        cout<<"3)delete element"<<endl;
         cin>>i;
 
         switch (i)
         {
         case 1:
+        {
             cout<<"enter data to add: "<<endl;
             int data;
             cin>>data;
             bstree->add(data);
             break;
+        }   
         case 2:
             bstree->inorderList();
             break;
+        case 3:
+        {
+           cout<<"enter data to remove: ";
+            int data;
+            cin>>data;
+            bstree->removeElement(data);
+            break; 
+        }
+            
         default:
             break;
         }
